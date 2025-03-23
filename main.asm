3d7195c61ab38bbaa73ad2bb5907ed66f358512c
David J. Sankel <dsankel@adobe.com>
2025-03-23 15:07:50 -0400

    Shortened refresh implementation and made it strobe between digits.

--- main.asm
+++ main.asm
@@ -86,6 +86,9 @@ shiftRegister:
 
 display_buffer:
     DS 4
+
+next_digit:
+    DS 1
     
 PSECT code
 
@@ -118,37 +121,79 @@ drawHex16:
     
     RETURN
 
-// Render one frame to the display of `display_buffer`
+// Return the high byte of the output port corresponding to the specified digit
+// (0 counted)
+ssdh_from_digit:
+    ADDWF PCL,F
+    RETLW SSDH_D1 ; TODO: rename these starting with 0
+    RETLW SSDH_D2
+    RETLW SSDH_D3
+    RETLW SSDH_D4
+    
+// Render one frame to the display of `display_buffer`. A single frame
+// corresponds to a single digit. This function, when called repeatedly, will
+// strobe between the different digits.
 refresh:
-    MOVLW SSDH_D1
-    MOVWF shiftRegister    
-    MOVF display_buffer,W
+    // DECF next_digit, W
+    MOVF next_digit, W
+    CALL ssdh_from_digit
+    MOVWF shiftRegister  
+    MOVLW display_buffer
+    ADDWF next_digit,W
+    // ADDLW -1
+    MOVWF FSR
+    MOVF INDF, W
     MOVWF shiftRegister+1
+    
     MOVLW shiftRegister
     CALL setOutput
     
-    MOVLW SSDH_D2
-    MOVWF shiftRegister    
-    MOVF display_buffer+1,W
-    MOVWF shiftRegister+1
-    MOVLW shiftRegister
-    CALL setOutput
-    
-    MOVLW SSDH_D3
-    MOVWF shiftRegister    
-    MOVF display_buffer+2,W
-    MOVWF shiftRegister+1
-    MOVLW shiftRegister
-    CALL setOutput
-    
-    MOVLW SSDH_D4
-    MOVWF shiftRegister    
-    MOVF display_buffer+3,W
-    MOVWF shiftRegister+1
-    MOVLW shiftRegister
-    CALL setOutput
-    
+
+    // Option 1: Fix value before decrementing the counter. Counter ranges
+    // beteen 0 and 3. Uses 6 instructions.
+     
+    MOVF next_digit, W
+    BTFSC ZERO
+    MOVLW 0x04
+    MOVWF  next_digit
+    DECF next_digit, F
     RETURN
+    
+    /*
+    // Option 2: Fix value after decrementing the counter. Counter ranges
+    // between 1 and 4. Uses 5 instructions, but requires 2 elsewhere.
+    DECFSZ next_digit,F
+    RETURN
+    MOVLW 0x04
+    MOVWF next_digit
+    RETURN
+    */
+    
+    /*
+    // Option 3: Explicitly handle base case when incrementing the counter.
+    // Counter ranges between 0 and 3. Uses 8 instructions.
+    MOVLW 0x03
+    SUBWF next_digit,W
+    BTFSC ZERO
+    GOTO foo
+    INCF next_digit,F
+    RETURN
+foo:
+    CLRF next_digit
+    RETURN
+    */
+    
+    /*
+    // Option 4: Fix value (cleverly) after incrementing the counter. Counter
+    // ranges between 0 and 3. Uses 7 instructions.
+    MOVLW 0xFD
+    ADDWF next_digit, F
+    BTFSC ZERO
+    MOVLW 0x01
+    ADDLW -1
+    SUBWF next_digit, F
+    RETURN
+     */
     
 delay:
     BTFSS TMR0IF
@@ -228,6 +273,8 @@ main:
     BCF TRIS_SER ; Set RA's 0-2 to output mode
     BCF TRIS_RCLK
     BCF TRIS_SRCLK
+    
+    CLRF next_digit
 
     MOVLW 0xFA
     MOVWF number
